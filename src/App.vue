ecd7c976f708ac5788d033c5a3585247f69082f2
TheAkshitS <akshit.s@outlook.com>
2023-09-11 07:53:09 +0530

    feat: fetchCurrentUser + fetchConversations

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <script setup>
+import { onMounted, computed } from 'vue';
+import { useUserStore } from './stores/UserStore';
+import { useConversationStore } from './stores/ConversationStore';
 
+const userStore = useUserStore();
+const conversationStore = useConversationStore();
+
+const fetchCurrentUser = async () => {
+  await userStore.fetchCurrentUser();
+};
+
+const fetchConversations = async () => {
+  await conversationStore.fetchConversations();
+};
+
+const currentUser = computed(() => userStore.currentUser);
+
+onMounted(() => {
+  Promise.all([fetchCurrentUser(), fetchConversations()]);
+});
 </script>
 
 <template>
-  <div>
-    App
-</div>
+  <div class="app__title">
+    <span v-if="currentUser.firstName">
+      Welcome back, {{ currentUser.firstName }}!
+    </span>
+  </div>
 </template>
 
 <style scoped>
-
+.app__title {
+  font-size: 2rem;
+  padding: 2rem;
+}
 </style>
